038d703f30c48c4be03188d6b4ae9b3c91542222
Andrew Murray <amurray@thegoodpenguin.co.uk>
2024-02-23 18:21:48 +0000

    Initial version
    This provides support for uploading a Yocto generated CVE json file
    to Galapagos

---
+++ scripts/send-galapagos-yocto-cves.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import requests
+import argparse
+
+parser = argparse.ArgumentParser(description="Utility to send CVE reports to Galapagos")
+
+parser.add_argument("cve_json", help="Yocto generated CVE .json file")
+parser.add_argument("product_name", help="Product name")
+parser.add_argument("product_key", help="Product API key")
+parser.add_argument("email", help="Email")
+parser.add_argument("interval", help="Min interval of report: build, daily or weekly")
+
+args = parser.parse_args()
+
+url = "https://galapagos.thegoodpenguin.co.uk/upload"
+
+files = {"yocto_cves": open(args.cve_json, "rb")}
+headers = {"Product-Key": args.product_key}
+data = {"email": args.email, "product": args.product_name, "interval": args.interval}
+
+r = requests.post(url, headers=headers, data=data, files=files)
+print(r.text)
