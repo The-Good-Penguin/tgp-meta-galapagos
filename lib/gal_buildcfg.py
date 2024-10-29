1a1ed7d8cc2367fe13a2ca326fd68f6dacc8cec2
Marc Kelly <mkelly@thegoodpenguin.co.uk>
2024-10-29 16:35:11 +0000

    Add Layer information collection function.
    Generates a json file similar to
    
    {
        "layers": {
            "meta": {
                "url": [
                    "https://git.yoctoproject.org/git/poky"
                ],
                "branch": "scarthgap",
                "revision": "cd44e6bd40b0c1f498b3feaeb5e9b72f8bf32d41",
                "tags": "yocto-5.0.4-38-gcd44e6bd40"
            },
            "meta-intel": {
                "url": [
                    "https://git.yoctoproject.org/meta-intel"
                ],
                "branch": "scarthgap",
                "revision": "c2bc8e27e9cc83654a23e8d89525fd9b1e781eb6",
                "tags": "21.0-scarthgap-5.0-19-gc2bc8e27"
            },
            "meta-virtualization": {
                "url": [
                    "https://git.yoctoproject.org/meta-virtualization"
                ],
                "branch": "scarthgap",
                "revision": "6f3c1d8f90947408a6587be222fec575a1ca5195"
            }
        }
    }
    
    Signed-off-by: Marc Kelly <mkelly@thegoodpenguin.co.uk>

---
+++ lib/gal_buildcfg.py
@@ -0,0 +1,42 @@
+# These are re-implemented to match the way Scarthgap does things
+# Only handles git currently
+import os
+import subprocess
+import bb.process
+
+def get_metadata_branch(path):
+    try:
+        rev, _ = bb.process.run('git rev-parse --abbrev-ref HEAD', cwd=path)
+    except bb.process.ExecutionError:
+        rev = '<unknown>'
+    return rev.strip()
+
+def get_metadata_revision(path):
+    try:
+        rev, _ = bb.process.run('git rev-parse HEAD', cwd=path)
+    except bb.process.ExecutionError:
+        rev = '<unknown>'
+    return rev.strip()
+
+def get_metadata_remotes(path):
+    try:
+        remotes_list, _ = bb.process.run('git remote', cwd=path)
+        remotes = remotes_list.split()
+    except bb.process.ExecutionError:
+        remotes = []
+    return remotes
+
+def get_metadata_remote_url(path, remote):
+    try:
+        uri, _ = bb.process.run('git remote get-url {remote}'.format(remote=remote), cwd=path)
+    except bb.process.ExecutionError:
+        return ""
+    return uri.strip()
+
+def get_metadata_describe(path):
+    try:
+        describe, _ = bb.process.run('git describe --tags', cwd=path)
+    except bb.process.ExecutionError:
+        return ""
+    return describe.strip()
+
