4b98a5a4b6dc1e915a1d898dae98421370bf10d5
Marc Kelly <mkelly@thegoodpenguin.co.uk>
2024-10-29 16:39:02 +0000

    Add Layer information collection function.
    Generates a json file similar to
    
    {
        "layers": {
            "meta": {
                "url": [
                    "https://git.yoctoproject.org/git/poky"
                ],
                "branch": "scarthgap",
                "revision": "cd44e6bd40b0c1f498b3feaeb5e9b72f8bf32d41",
                "tags": "yocto-5.0.4-38-gcd44e6bd40"
            },
            "meta-intel": {
                "url": [
                    "https://git.yoctoproject.org/meta-intel"
                ],
                "branch": "scarthgap",
                "revision": "c2bc8e27e9cc83654a23e8d89525fd9b1e781eb6",
                "tags": "21.0-scarthgap-5.0-19-gc2bc8e27"
            },
            "meta-virtualization": {
                "url": [
                    "https://git.yoctoproject.org/meta-virtualization"
                ],
                "branch": "scarthgap",
                "revision": "6f3c1d8f90947408a6587be222fec575a1ca5195"
            }
        }
    }
    
    Signed-off-by: Marc Kelly <mkelly@thegoodpenguin.co.uk>

---
+++ lib/gal_buildcfg.py
@@ -0,0 +1,19 @@
+# This is a basic wrapper for Scarthgap and later around oe.buildcfg
+
+import oe.buildcfg
+
+def get_metadata_branch(path):
+    return oe.buildcfg.get_metadata_git_branch(path)
+
+def get_metadata_revision(path):
+    return oe.buildcfg.get_metadata_git_revision(path)
+
+def get_metadata_remotes(path):
+    return oe.buildcfg.get_metadata_git_remotes(path)
+
+def get_metadata_remote_url(path, remote):
+    return oe.buildcfg.get_metadata_git_remote_url(path, remote)
+
+def get_metadata_describe(path):
+    return oe.buildcfg.get_metadata_git_describe(path)
+
